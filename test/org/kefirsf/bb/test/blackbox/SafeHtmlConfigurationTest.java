35b066c2266d5fb3468c5b6f5fb90f77b891ecb8
Vitalii Samolovskikh <kefir@perm.ru>
2013-02-17 12:23:08 +0600

    #2 Add safe HTML headers. And some tests.

--- test/org/kefirsf/bb/test/blackbox/SafeHtmlConfigurationTest.java
+++ test/org/kefirsf/bb/test/blackbox/SafeHtmlConfigurationTest.java
@@ -30,6 +30,37 @@ public class SafeHtmlConfigurationTest {
 
     @Test
     public void test(){
+        // Safe XML
+        assertProcess("&lt;", "<");
+        assertProcess("&gt;", ">");
+        assertProcess("&apos;", "\'");
+        assertProcess("&quot;", "\"");
+
+        // Escape HTML
+        assertProcess("&lt;", "\\<");
+        assertProcess("&gt;", "\\>");
+        assertProcess("&amp;", "\\&");
+        assertProcess("\\", "\\\\");
+        assertProcess("", "<!-- Comment -->");
+
+        // Line breaks
+        assertProcess("<br/>", "\r\n");
+        assertProcess("<br/>", "\n\r");
+        assertProcess("<br/>", "\r");
+        assertProcess("<br/>", "\n");
+
+        assertProcess("<br/><br/>", "\r\n\r\n");
+        assertProcess("<br/><br/>", "\n\r\n\r");
+
+        // Headers
+        assertProcess("<h1>Test</h1>", "<H1 onclick=\"...\">Test</h1>");
+        assertProcess("<h2>Test</h2>", "<h2 onclick=\"...\">Test</H2>");
+        assertProcess("<h3>Test</h3>", "<H3 onclick=\"...\">Test</H3>");
+        assertProcess("<h4>Test</h4>", "<H4 onclick=\"...\">Test</h4>");
+        assertProcess("<h5>Test</h5>", "<H5 onclick=\"...\">Test</h5>");
+        assertProcess("<h6>Test</h6>", "<H6 onclick=\"...\">Test</h6>");
+
+        // Simple HTML
         assertProcess("<b>test</b>", "<b onclick=\"javascript:alert('Fail!');\">test</B>");
     }
 }
