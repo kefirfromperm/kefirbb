e40ff15f4334152d2bcb4bfb51ea7be6355cf890
Vitalii Samolovskikh <kefir@perm.ru>
2013-02-24 15:25:07 +0600

    #6 Tests for configuration class Scope.

--- test/org/kefirsf/bb/conf/ScopeTest.java
+++ test/org/kefirsf/bb/conf/ScopeTest.java
@@ -3,6 +3,9 @@ package org.kefirsf.bb.conf;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 /**
  * Tests for configuration class Scope.
  *
@@ -51,5 +54,53 @@ public class ScopeTest {
         Assert.assertEquals(true, scope.isIgnoreText());
         Assert.assertNotNull(scope.getCodes());
         Assert.assertTrue(scope.getCodes().isEmpty());
+    }
+
+    @Test
+    public void testName() {
+        Scope scope = new Scope();
+        scope.setName(SCOPE_NAME);
+        Assert.assertEquals(SCOPE_NAME, scope.getName());
+    }
+
+    @Test
+    public void testParent() {
+        Scope scope = new Scope();
+        Scope parent = new Scope();
+        scope.setParent(parent);
+        Assert.assertEquals(parent, scope.getParent());
+    }
+
+    @Test
+    public void testIgnoreText() {
+        Scope scope = new Scope();
+        scope.setIgnoreText(true);
+        Assert.assertEquals(true, scope.isIgnoreText());
+    }
+
+    @Test
+    public void testCodes() {
+        Scope scope = new Scope();
+
+        Code code = new Code();
+        code.setPattern(new Pattern(Arrays.asList(
+                new Constant("["), new Text("val", scope, false), new Constant("]")
+        )));
+        code.setTemplate(new Template(Arrays.asList(new NamedValue("val"))));
+
+        scope.setCodes(new HashSet<Code>(Arrays.asList(code)));
+
+        Assert.assertEquals(1, scope.getCodes().size());
+        Assert.assertTrue(scope.getCodes().contains(code));
+
+        code = new Code();
+        code.setPattern(new Pattern(Arrays.asList(
+                new Constant("*"), new Text("val", scope, false), new Constant("*")
+        )));
+        code.setTemplate(new Template(Arrays.asList(new NamedValue("val"))));
+        scope.addCode(code);
+
+        Assert.assertEquals(2, scope.getCodes().size());
+        Assert.assertTrue(scope.getCodes().contains(code));
     }
 }
