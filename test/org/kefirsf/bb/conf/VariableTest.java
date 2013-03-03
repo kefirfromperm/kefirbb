7e81b1967d78ce52d46fe8d41919c7f0b5a78548
Vitalii Samolovskikh <kefir@perm.ru>
2013-03-03 15:34:58 +0600

    #5 #6 #7 Constant named values

--- test/org/kefirsf/bb/conf/VariableTest.java
+++ test/org/kefirsf/bb/conf/VariableTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 /**
  * Tests for configuration class Variable.
+ *
  * @author Vitalii Samolovskikh aka Kefir
  */
 public class VariableTest {
@@ -13,35 +14,35 @@ public class VariableTest {
     public static final java.util.regex.Pattern REGEX = java.util.regex.Pattern.compile("\\d+");
 
     @Test
-    public void testConstructor1(){
+    public void testConstructor1() {
         Variable variable = new Variable();
         Assert.assertEquals(null, variable.getName());
         Assert.assertEquals(null, variable.getRegex());
     }
 
     @Test
-    public void testConstructor2(){
+    public void testConstructor2() {
         Variable variable = new Variable(NAME);
         Assert.assertEquals(NAME, variable.getName());
         Assert.assertEquals(null, variable.getRegex());
     }
 
     @Test
-    public void testConstructor3(){
+    public void testConstructor3() {
         Variable variable = new Variable(NAME, REGEX);
         Assert.assertEquals(NAME, variable.getName());
         Assert.assertEquals(REGEX, variable.getRegex());
     }
 
     @Test
-    public void testName(){
+    public void testName() {
         Variable variable = new Variable();
         variable.setName(NAME);
         Assert.assertEquals(NAME, variable.getName());
     }
 
     @Test
-    public void testRegex(){
+    public void testRegex() {
         Variable variable = new Variable();
         variable.setRegex(REGEX);
         Assert.assertEquals(REGEX, variable.getRegex());
