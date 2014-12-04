c0351c4fd515f9aee195ee9dd58555994ab0b574
Vitalii Samolovskikh <kefir@perm.ru>
2014-12-04 23:34:52 +0500

    findIn in BOL

--- src/org/kefirsf/bb/proc/ProcBol.java
+++ src/org/kefirsf/bb/proc/ProcBol.java
@@ -12,26 +12,68 @@ public class ProcBol implements ProcPatternElement {
 
     public boolean isNextIn(Source source) {
         int offset = source.getOffset();
-        if (offset == 0) {
-            char c = source.charAt(offset);
-            if(c!='\n' && c!='\r') {
-                return true;
-            }
-        } else {
-            char p = source.charAt(offset - 1);
-            char c = source.charAt(offset);
-            if ((p == '\n' || p == '\r') && c!='\n' && c!='\r') {
-                return true;
+        int length = source.length();
+        if (offset < length) {
+            if (offset == 0) {
+                char c = source.charAt(offset);
+                if (c != '\n' && c != '\r') {
+                    return true;
+                }
+            } else {
+                char p = source.charAt(offset - 1);
+                char c = source.charAt(offset);
+                if ((p == '\n' || p == '\r') && c != '\n' && c != '\r') {
+                    return true;
+                }
             }
         }
         return false;
     }
 
     /**
-     * I strongly don't recommend to use tag sol as a terminator.
+     * I strongly don't recommend to use tag bol as a terminator.
      */
     public int findIn(Source source) {
-        // TODO Add an implementation.
-        throw new UnsupportedOperationException("The operation findIn is unsupported for beginning of line.");
+        int offset = source.getOffset();
+        int length = source.length();
+        if (offset < length) {
+            if (offset == 0) {
+                char c = source.charAt(offset);
+                if (c != '\n' && c != '\r') {
+                    return 0;
+                }
+            } else {
+                char c = source.charAt(offset);
+                char p = source.charAt(offset - 1);
+                if (c != '\n' && c != '\r' && (p == '\n' || p == '\r')) {
+                    return offset;
+                }
+
+                int index;
+                int n = source.findFrom(offset, AbstractEol.AN, false);
+                int r = source.findFrom(offset, AbstractEol.AR, false);
+                if (n >= 0 && r >= 0) {
+                    index = Math.min(n, r);
+                } else if (n >= 0) {
+                    index = n;
+                } else if (r >= 0) {
+                    index = r;
+                } else {
+                    return -1;
+                }
+
+                // Whitespaces
+                while (index < source.length() && !lineBreak(source.charAt(index))) {
+                    index++;
+                }
+
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    private boolean lineBreak(double c) {
+        return (c == '\n' || c == '\r');
     }
 }
