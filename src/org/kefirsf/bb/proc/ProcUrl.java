2e025568511f5eef8037b5cbfe012af8c3575dc0
Vitalii Samolovskikh <kefir@perm.ru>
2015-03-22 16:18:28 +0500

    #12 A tag for URL started

---
+++ src/org/kefirsf/bb/proc/ProcUrl.java
@@ -0,0 +1,99 @@
+package org.kefirsf.bb.proc;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Parse an URL
+ * @author kefir
+ */
+public class ProcUrl extends ProcNamedElement implements ProcPatternElement {
+    private static final Pattern pattern = Pattern.compile(
+            "((ht|f)tps?://)?(\\w+\\.)+\\w{2,}(/\\w+)*"
+    );
+    boolean ghost = false;
+
+    public ProcUrl(String name) {
+        super(name);
+    }
+
+    /**
+     * Parse element
+     *
+     * @param context    context
+     * @param terminator teminator to stop text process
+     * @return true - subsequence is valid to this pattern
+     * false - not valid
+     */
+    public boolean parse(Context context, ProcPatternElement terminator) throws NestingException {
+        Source source = context.getSource();
+        int offset = source.getOffset();
+
+        int end;
+        if (terminator != null) {
+            end = terminator.findIn(context.getSource());
+            if (end < 0) {
+                end = context.getSource().length();
+            }
+        } else {
+            end = context.getSource().length();
+        }
+
+
+        CharSequence value = source.sub(end);
+
+        // find this regex in value
+            Matcher matcher = pattern.matcher(value);
+            if (matcher.lookingAt()) {
+                int lend = matcher.end();
+                end = offset + lend;
+                value = matcher.group();
+            } else {
+                return false;
+            }
+
+        // Test this variable already defined and equals with this in this code scope
+        Object attr = context.getLocalAttribute(getName());
+        if (attr == null || attr.equals(value)) {
+            if (attr == null) {
+                setAttribute(context, value);
+            }
+            if(!ghost) {
+                source.incOffset(end - offset);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Check next subsequence
+     *
+     * @param source source text
+     * @return true pattern sequence equals with next subsequence
+     * false not equals
+     */
+    public boolean isNextIn(Source source) {
+        return pattern.matcher(source.subToEnd()).lookingAt();
+    }
+
+    /**
+     * Find constant
+     *
+     * @param source text source
+     * @return constant offset
+     */
+    public int findIn(Source source) {
+        if (pattern != null) {
+            Matcher matcher = pattern.matcher(source.subToEnd());
+            if (matcher.find()) {
+                return source.getOffset() + matcher.start();
+            } else {
+                return -1;
+            }
+        } else {
+            return -1;
+        }
+    }
+}
---
+++ test/org/kefirsf/bb/proc/ProcUrlTest.java
@@ -0,0 +1,41 @@
+package org.kefirsf.bb.proc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests for the URL pattern element.
+ *
+ * @author kefir
+ * @see org.kefirsf.bb.proc.ProcUrl
+ */
+public class ProcUrlTest {
+    private static final String SIMPLE_URL = "http://www.example.com";
+    private static final String PREFIX = " ";
+    private static final String SUFFIX = ".";
+    private static final String VAR_NAME = "url";
+
+    @Test
+    public void testParse() throws NestingException {
+        Source source = new Source(SIMPLE_URL + SUFFIX);
+        Context context = new Context();
+        context.setSource(source);
+        ProcUrl el = new ProcUrl(VAR_NAME);
+        assertTrue(el.parse(context, null));
+        assertEquals(SIMPLE_URL, context.getAttribute(VAR_NAME));
+    }
+
+    @Test
+    public void testIsNextIn() {
+        ProcUrl el = new ProcUrl(VAR_NAME);
+        assertTrue(el.isNextIn(new Source(SIMPLE_URL)));
+    }
+
+    @Test
+    public void findIn() {
+        ProcUrl el = new ProcUrl(VAR_NAME);
+        assertEquals(PREFIX.length(), el.findIn(new Source(PREFIX + SIMPLE_URL + SUFFIX)));
+    }
+}
