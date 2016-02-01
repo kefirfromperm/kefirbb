df1e0b322f23b7c96aca6ca544b0ccf813245fbb
Vitalii Samolovskikh <Vitaliy.Samolovskikh@aplana.com>
2016-02-01 15:53:05 +0500

    #12 Programming config  for local and schemaless URLs

--- src/org/kefirsf/bb/conf/Url.java
+++ src/org/kefirsf/bb/conf/Url.java
@@ -9,9 +9,19 @@ public class Url extends NamedElement implements PatternElement {
     private boolean ghost = false;
 
     /**
+     * Accept local URLs.
+     */
+    private boolean local = false;
+
+    /**
+     * Accept URLs without a schema. This URL can't be terminator and can't accept URLs with a schema.
+     */
+    private boolean schemaless = false;
+
+    /**
      * Create an URL pattern element.
      *
-     * @param name variable name
+     * @param name  variable name
      * @param ghost is it ghost?
      */
     public Url(String name, boolean ghost) {
@@ -33,5 +43,45 @@ public class Url extends NamedElement implements PatternElement {
      */
     public void setGhost(boolean ghost) {
         this.ghost = ghost;
+    }
+
+    /**
+     * Local URLs a URLs without a schema and host
+     *
+     * @return true - if the URL can accept local URLs also,
+     * false - if URL must be absolute.
+     */
+    public boolean isLocal() {
+        return local;
+    }
+
+    /**
+     * Set support local URLs.
+     * <p/>
+     * true - if the URL can accept local URLs also,
+     * false - if URL must be absolute.
+     *
+     * @param local new value
+     */
+    public void setLocal(boolean local) {
+        this.local = local;
+    }
+
+    /**
+     * Is it URL without a schema? Like www.example.com
+     *
+     * @return true if accept URLs only without a schema, false otherwise
+     */
+    public boolean isSchemaless() {
+        return schemaless;
+    }
+
+    /**
+     * Set this is URL with schema or not?
+     *
+     * @param schemaless if true then without schema.
+     */
+    public void setSchemaless(boolean schemaless) {
+        this.schemaless = schemaless;
     }
 }
