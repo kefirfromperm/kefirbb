3b8337f53ab92468f34fc06fa7f83584c51f05cf
Vitalii Samolovskikh <kefirfromperm@gmail.com>
2016-03-12 23:30:52 +0500

    Fix performance random test

--- test/org/kefirsf/bb/test/PerformanceRandomTest.java
+++ test/org/kefirsf/bb/test/PerformanceRandomTest.java
@@ -1,7 +1,9 @@
 package org.kefirsf.bb.test;
 
 import org.kefirsf.bb.BBProcessorFactory;
+import org.kefirsf.bb.ConfigurationFactory;
 import org.kefirsf.bb.TextProcessor;
+import org.kefirsf.bb.conf.Configuration;
 
 import java.text.DecimalFormat;
 import java.text.MessageFormat;
@@ -40,7 +42,10 @@ public class PerformanceRandomTest {
         }
         String text = builder.toString();
 
-        TextProcessor processor = BBProcessorFactory.getInstance().createFromResource("org/kefirsf/bb/default.xml");
+        Configuration conf = ConfigurationFactory.getInstance().createFromResource("org/kefirsf/bb/default.xml");
+        conf.setPropagateNestingException(true);
+        conf.setNestingLimit(1000);
+        TextProcessor processor = BBProcessorFactory.getInstance().create(conf);
 
         long start = System.currentTimeMillis();
         String result = processor.process(text);
@@ -55,6 +60,6 @@ public class PerformanceRandomTest {
                 MessageFormat.format("Time: {0} milliseconds.", format.format(finish - start))
         );
 
-        System.out.println(MessageFormat.format("Result: {0}", result.substring(0, 256)));
+        System.out.println(MessageFormat.format("Result: {0}", result.substring(0, Math.min(256, result.length()))));
     }
 }
