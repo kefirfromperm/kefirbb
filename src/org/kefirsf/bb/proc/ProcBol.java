f4797f4e9677f67005b7e4e6a0b26c8ee60c3cb8
Vitalii Samolovskikh <Vitaliy.Samolovskikh@aplana.com>
2016-04-20 12:13:35 +0500

    Optimize ProcBol

--- src/org/kefirsf/bb/proc/ProcBol.java
+++ src/org/kefirsf/bb/proc/ProcBol.java
@@ -14,21 +14,7 @@ public class ProcBol implements ProcPatternElement {
         Source source = context.getSource();
         int offset = source.getOffset();
         int length = source.length();
-        if (offset < length) {
-            if (offset == 0) {
-                char c = source.charAt(offset);
-                if (c != '\n' && c != '\r') {
-                    return true;
-                }
-            } else {
-                char p = source.charAt(offset - 1);
-                char c = source.charAt(offset);
-                if ((p == '\n' || p == '\r') && c != '\n' && c != '\r') {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return offset < length && onBol(source, offset);
     }
 
     /**
@@ -38,44 +24,91 @@ public class ProcBol implements ProcPatternElement {
         int offset = source.getOffset();
         int length = source.length();
         if (offset < length) {
-            if (offset == 0) {
-                char c = source.charAt(offset);
-                if (c != '\n' && c != '\r') {
-                    return 0;
-                }
-            } else {
-                char c = source.charAt(offset);
-                char p = source.charAt(offset - 1);
-                if (c != '\n' && c != '\r' && (p == '\n' || p == '\r')) {
-                    return offset;
-                }
-
-                int index;
-                int n = source.findFrom(offset, AbstractEol.AN, false);
-                int r = source.findFrom(offset, AbstractEol.AR, false);
-                if (n >= 0 && r >= 0) {
-                    index = Math.min(n, r);
-                } else if (n >= 0) {
-                    index = n;
-                } else if (r >= 0) {
-                    index = r;
-                } else {
-                    return -1;
-                }
-
-                // Whitespaces
-                while (index < source.length() && !lineBreak(source.charAt(index))) {
-                    index++;
-                }
-
-                return index;
+            // maybe we are on the beginning of line already?
+            if (onBol(source, offset)) {
+                return offset;
             }
+
+            // If no find next line break
+            int index = findLineBreak(source, offset);
+
+            if (index < 0) {
+                return -1;
+            }
+
+            // Skip line break 1 or 2 characters
+            index = skipLineBreak(source, index);
+
+            // If index is not on the end of text return it otherwise it's not a beginning of line.
+            if (index < length) {
+                return index;
+            } else {
+                return -1;
+            }
+        } else {
+            return -1;
         }
-        return -1;
     }
 
-    private boolean lineBreak(double c) {
-        return (c == '\n' || c == '\r');
+    /**
+     * Check we are on the beginning of line
+     *
+     * @param source the text source
+     * @param offset the offset
+     * @return true if we are on the beginning of line
+     */
+    private boolean onBol(Source source, int offset) {
+        if (offset == 0) {
+            return true;
+        } else {
+            char c = source.charAt(offset);
+            char p = source.charAt(offset - 1);
+            return c != '\n' && c != '\r' && (p == '\n' || p == '\r');
+        }
+    }
+
+    /**
+     * Find first input of line break
+     *
+     * @param source the text source
+     * @param offset current offset
+     */
+    private int findLineBreak(Source source, int offset) {
+        int index;
+        int n = source.findFrom(offset, AbstractEol.AN, false);
+        int r = source.findFrom(offset, AbstractEol.AR, false);
+        if (n >= 0 && r >= 0) {
+            index = Math.min(n, r);
+        } else if (n >= 0) {
+            index = n;
+        } else if (r >= 0) {
+            index = r;
+        } else {
+            index = -1;
+        }
+        return index;
+    }
+
+    /**
+     * Skip line break characters. '\r\n' for example
+     *
+     * @param source text source
+     * @param offset current index
+     * @return new index
+     */
+    private int skipLineBreak(Source source, final int offset) {
+        char p;
+        char c;
+        int index = offset;
+        p = source.charAt(index);
+        index++;
+        if (index < source.length()) {
+            c = source.charAt(index);
+            if ((c == '\n' || c == '\r') && c != p) {
+                index++;
+            }
+        }
+        return index;
     }
 
     @Override
